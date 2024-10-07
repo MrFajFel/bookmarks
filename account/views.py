f6a970f02b95cf1ba0d534ef34c512b662f8d452
skiparb <skiparb@gmail.com>
2024-10-07 10:27:52 +0200

    Zrobiony html i zaczete logowanie

--- account/views.py
+++ account/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from account.form import Loginform
+
+def user_login(request):
+    if request.method == "POST":
+        pass
+    else:
+        form = Loginform()
+    return  render(request,'account/login.html',{'form': form})
+
+
 # Create your views here.
